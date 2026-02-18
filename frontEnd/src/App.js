07e4fac2fe81c4ca8df27db22ba26d1da5db2cf3
prasadproject-pbl <prasadproject-pbl>
2026-02-18 14:48:06 +0530

    update local host to

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -8,7 +8,7 @@ import Sidebar from "./Sidebar";
 
 import "./style.css";
 
-const server = "http://localhost:5000";
+const server = process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
 const connectionOptions = {
   "force new connection": true,
   reconnectionAttempts: "Infinity",
